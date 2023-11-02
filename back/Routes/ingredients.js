79f11636592f46c813f2892d9dfc517251c51ad5
Aurelien Boets <aurelien.boets59@gmail.com>
2023-11-02 11:11:29 +0100

    create routes

--- back/Routes/ingredients.js
+++ back/Routes/ingredients.js
@@ -1,21 +1,48 @@
-import { IngredientDao } from "../Dao/IngredientDao";
+import { IngredientDao } from "../Dao/IngredientDao.js";
+import { Ingredient } from "../Model/Ingredient.js";
+import { authMiddleware } from "../Middleware/middleware.js";
 import express from "express";
 import cors from "cors";
 
-const ingredientsDao = new IngredientDao();
+const ingredientDao = new IngredientDao();
 const ingredients = express.Router();
 ingredients.use(express.json());
 ingredients.use(cors());
 
 ingredients.get("/", (req, res) => {
-  res.status(200).send(ingredientsDao.getAll());
+  res.status(200).send(ingredientDao.getAll());
 });
 
 ingredients.get("/:id", (req, res) => {
-  const ingredient = ingredientsDao.getById(req.params.id);
+  const ingredient = ingredientDao.getById(req.params.id);
   if (ingredient) {
     res.status(200).send(ingredient);
   } else {
     res.sendStatus(404);
   }
 });
+
+ingredients.post("/", authMiddleware, (req, res) => {
+  const name = req.body.name;
+  const ingredient = new Ingredient(name);
+  ingredientDao.addIngredient(ingredient);
+  res.sendStatus(201);
+});
+
+ingredients.put("/:id", authMiddleware, (req, res) => {
+  let newIngredient = req.body;
+  newIngredient.id = req.params.id;
+  const isUpdate = ingredientDao.edit(newIngredient);
+  if (isUpdate) {
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+ingredients.delete("/:id", authMiddleware, (req, res) => {
+  ingredientDao.remove(req.params.id);
+  res.sendStatus(204);
+});
+
+export default ingredients;
---
+++ back/Routes/recipes.js
@@ -0,0 +1,46 @@
+import express from "express";
+import cors from "cors";
+import { RecipeDao } from "../Dao/RecipeDao.js";
+import { Recipe } from "../Model/Recipe.js";
+import { authMiddleware } from "../Middleware/middleware.js";
+
+const recipeDao = new RecipeDao();
+const recipes = express.Router();
+recipes.use(cors());
+recipes.use(express.json());
+
+recipes.get("/", (req, res) => {
+  res.send(recipeDao.getAll());
+});
+
+recipes.get("/:id", (req, res) => {
+  const recipe = recipeDao.getById(req.params.id);
+  if (recipe) {
+    res.send(recipe);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+recipes.post("/", authMiddleware, (req, res) => {
+  const { name, cookTime, prepTime, instructions, ingredients } = req.body;
+  let recipe = new Recipe(name, cookTime, prepTime, instructions, ingredients);
+  recipeDao.addRecipe(recipe);
+  res.send(recipe).status(201);
+});
+
+recipes.put("/:id", authMiddleware, (req, res) => {
+  let newRecipe = req.body;
+  newRecipe.id = req.params.id;
+  const isUpdate = recipeDao.edit(newRecipe);
+  if (isUpdate) {
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+recipes.delete("/:id", authMiddleware, (req, res) => {
+  recipeDao.remove(req.params.id);
+  res.sendStatus(204);
+});
